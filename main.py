d6616a6cfdd3f9e12619b99b4dc3d692ef209121
Ty Simpson <70077620+ths1990@users.noreply.github.com>
2023-03-01 12:00:30 -0500

    - Created a for loop to iterate through projects - Create a function to add to invoice dictionary of projects and payout
    - To Fix: Pay format does not match currency at this point in time.

--- main.py
+++ main.py
@@ -1,36 +1,34 @@
 def main() -> object:
-    invoices = []  # Array list to store invoice totals
+    def calc_hours(hours_worked, minutes_worked):
+        """Function to convert hours and minutes worked into a number that can be used in pay calculation"""
+        hours_minutes = hours_worked + minutes_worked / 60
+        return hours_minutes
 
+    def calc_pay(time, hourly_rate):
+        """Function to calculate total pay"""
+        total_pay = time * hourly_rate
+        return total_pay
 
-def calc_hours(hours_worked, minutes_worked):
-    """Function to convert hours and minutes worked into a number that can be used in pay calculation"""
-    hours_minutes = hours_worked + minutes_worked / 60
-    return hours_minutes
+    invoices = {}  # Dictionary to store projects and invoice totals
+    num_projects = int(input("How many projects are you invoicing for?"))
 
+    for i in range(num_projects):
+        project_name = input("What is the name of your project?")
+        rate = float(input("What is the hourly rate for the project?"))
+        hours = int(input("How many hours did you work?"))
+        minutes = int(input("How many minutes did you work (0-59)?"))
 
-def calc_pay(time, hourly_rate):
-    """Function to calculate total pay"""
-    total_pay = time * hourly_rate
-    return total_pay
+        if 0 < minutes > 59:
+            print("Minutes must be between 0 and 59. Try again.")
+            minutes = int(input("How many minutes did you work (0-59)?"))
+        else:
+            total_time = calc_hours(hours, minutes)
 
+        pay = calc_pay(total_time, rate)
+        invoices[project_name] = pay
+    # TODO: Fix pay format to print out as $x.xx
+    print(invoices)
 
-# TODO: Add while loop to allow user to keep adding projects to create invoices for
-
-hours = int(input("How many hours did you work?"))
-minutes = int(input("How many minutes did you work (0-59)?"))
-rate = float(input("What is your hourly rate?"))
-
-if 0 < minutes > 59:
-    print("Minutes must be between 0 and 59. Try again.")
-    minutes = int(input("How many minutes did you work (0-59)?"))
-else:
-    total_time = calc_hours(hours, minutes)
-
-pay = calc_pay(total_time, rate)
-
-# TODO: Replace with function to add invoices to list/dictionary and print
-
-# print(pay) *Test Line to confirm calculation works
 
 if __name__ == '__main__':
     main()
