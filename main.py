e99e151941636b91d279d4094129ef1f0789f85b
Ty Simpson <70077620+ths1990@users.noreply.github.com>
2023-03-03 17:06:48 -0500

    - invoices now stored in nested dictionary -console print is now in a more readable format

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-def main():
+import decimal
 
+
+def main():
     def calc_hours(hours_worked, minutes_worked):
         """Function to convert hours and minutes worked into a number that can be used in pay calculation"""
-        hours_minutes = hours_worked + minutes_worked / 60
+        hours_minutes = decimal.Decimal(hours_worked + minutes_worked / 60)
         return hours_minutes
 
     def calc_pay(time, hourly_rate):
         """Function to calculate total pay"""
-        total_pay = time * hourly_rate
+        total_pay = decimal.Decimal(time * hourly_rate).quantize(decimal.Decimal('0.00'))
         return total_pay
 
     invoices = {}  # Dictionary to store projects and invoice totals
+    project_id = 0
     num_projects = int(input("How many projects are you invoicing for?"))
 
     for i in range(num_projects):
+        project_id = project_id + 1
+        invoices[project_id] = {}
         project_name = input("What is the name of your project?")
-        rate = float(input("What is the hourly rate for the project?"))
+        rate = decimal.Decimal(input("What is the hourly rate for the project?")).quantize(decimal.Decimal('0.00'))
         hours = int(input("How many hours did you work?"))
         minutes = int(input("How many minutes did you work (0-59)?"))
 
@@ -26,9 +31,19 @@ def main():
             total_time = calc_hours(hours, minutes)
 
         pay = calc_pay(total_time, rate)
-        invoices[project_name] = total_time, rate, pay
-    # TODO: Fix pay format to print out as $x.xx
-    print(invoices)
+
+        # Assigns key pair values for rate, total hours, and total pay
+        invoices[project_id]["Project Name"] = project_name
+        invoices[project_id]["Rate"] = rate
+        invoices[project_id]["Total Hours"] = total_time
+        invoices[project_id]["Total Pay"] = pay
+
+    for i, v in invoices.items():
+        print("\n")
+        print("Invoice --", i, "--\n")
+
+        for j, k in invoices[i].items():
+            print(j, ":", k)
 
 
 if __name__ == '__main__':
